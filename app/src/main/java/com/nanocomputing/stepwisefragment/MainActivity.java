ddaf6de59940166b3116152121f4408b4ffea0b7
nixswinner <nixondeveloper@gmail.com>
2018-07-21 12:52:52 +0300

    initial commit

---
+++ app/src/main/java/com/nanocomputing/stepwisefragment/MainActivity.java
@@ -0,0 +1,27 @@
+package com.nanocomputing.stepwisefragment;
+
+import android.net.Credentials;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.stepstone.stepper.StepperLayout;
+
+public class MainActivity extends AppCompatActivity
+        implements Personal_info.OnFragmentInteractionListener,
+        credentials.OnFragmentInteractionListener,
+complete_registrations.OnFragmentInteractionListener{
+    private StepperLayout mStepperLayout;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mStepperLayout = (StepperLayout) findViewById(R.id.stepperLayout);
+        mStepperLayout.setAdapter(new MyStepperAdapter(getSupportFragmentManager(), this));
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
